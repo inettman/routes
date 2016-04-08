1847f6af9d8d9614dec2a0bd667e72633837fa06
inettman <inettman@gmail.com>
2016-04-08 23:08:32 +0300

    changes

--- js/adMob.js
+++ js/adMob.js
@@ -1,47 +1,27 @@
 var admobid = {};
-
-// TODO: replace the following ad units with your own
-if( /(android)/i.test(navigator.userAgent) ) {
-    admobid = { // for Android
-        banner: 'ca-app-pub-0006856964532208/6283907611',
+if( /(android)/i.test(navigator.userAgent) ) { // for android & amazon-fireos
+    admobid = {
+        banner: 'ca-app-pub-0006856964532208/6283907611', // or DFP format "/6253334/dfp_example_ad"
         interstitial: 'ca-app-pub-0006856964532208/3073089213'
     };
-} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
-    admobid = { // for iOS
-        banner: 'ca-app-pub-0006856964532208/6283907611',
+} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) { // for ios
+    admobid = {
+        banner: 'ca-app-pub-0006856964532208/6283907611', // or DFP format "/6253334/dfp_example_ad"
         interstitial: 'ca-app-pub-0006856964532208/3073089213'
     };
-} else {
-    admobid = { // for Windows Phone
-        banner: 'ca-app-pub-0006856964532208/6283907611',
+} else { // for windows phone
+    admobid = {
+        banner: 'ca-app-pub-0006856964532208/6283907611', // or DFP format "/6253334/dfp_example_ad"
         interstitial: 'ca-app-pub-0006856964532208/3073089213'
     };
 }
 
-function initApp() {
+if(AdMob) AdMob.createBanner({
+    adId: admobid.banner,
+    position: AdMob.AD_POSITION.BOTTOM_CENTER,
+    autoShow: true });
 
-    if (! AdMob ) { alert( 'admob plugin not ready' ); return; }
+if(AdMob) AdMob.prepareInterstitial( {adId:admobid.interstitial, autoShow:false} );
 
-    // this will create a banner on startup
-    AdMob.createBanner( {
-        adId: admobid.banner,
-        position: AdMob.AD_POSITION.BOTTOM_CENTER,
-        isTesting: true, // TODO: remove this line when release
-        overlap: false,
-        offsetTopBar: false,
-        bgColor: 'black'
-    } );
-
-    // this will load a full screen ad on startup
-    AdMob.prepareInterstitial({
-        adId: admobid.interstitial,
-        isTesting: true, // TODO: remove this line when release
-        autoShow: true
-    });
-}
-
-if(( /(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent) )) {
-    document.addEventListener('deviceready', initApp, false);
-} else {
-    initApp();
-}
+// show the interstitial later, e.g. at end of game level
+if(AdMob) AdMob.showInterstitial();
